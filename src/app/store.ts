769975d3ab285dd7abd8f3287adb2b133626c4c9
pascal-la <lapascal89@gmail.com>
2024-06-03 11:19:37 +0200

    install zustand / create store

---
+++ src/app/store.ts
@@ -0,0 +1,28 @@
+import { create } from "zustand";
+
+interface AuthStore {
+  tokenName: string;
+  isLoggedIn: boolean;
+  login: (token: string) => void;
+  logout: () => void;
+}
+
+const TOKEN_NAME = "edulog_token";
+
+const getToken =
+  typeof window !== "undefined" && !!localStorage.getItem(TOKEN_NAME);
+
+const useAuthStore = create<AuthStore>((set) => ({
+  tokenName: TOKEN_NAME,
+  isLoggedIn: getToken,
+  login: (token) => {
+    set({ isLoggedIn: true });
+    localStorage.setItem(TOKEN_NAME, token);
+  },
+  logout: () => {
+    set({ isLoggedIn: false });
+    localStorage.removeItem(TOKEN_NAME);
+  },
+}));
+
+export default useAuthStore;
