6f9686640a45cdc40c36e1165a96f1d4fb5f32c0
pascal-la <lapascal89@gmail.com>
2024-06-03 13:12:15 +0200

    add styles in login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -54,24 +54,32 @@ export default function LoginPage() {
   }
 
   return (
-    <>
+    <div className="flex flex-col items-center gap-20">
       <Title title="Portail de sortie scolaire" />
-      <form ref={ref} action={signIn}>
+      <form ref={ref} action={signIn} className="grid gap-8 w-full max-w-2xl">
         {inputs.map((input) => (
           <div key={input.type}>
-            <label>{input.label}</label>
+            <label className="block text-sm font-semibold leading-6 text-gray-900">
+              {input.label}
+            </label>
             <div className="mt-2.5">
               <input
                 id={input.type}
                 type={input.type}
                 name={input.type}
                 required
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
           </div>
         ))}
-        <button type="submit">Se connecter</button>
+        <button
+          type="submit"
+          className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Se connecter
+        </button>
       </form>
-    </>
+    </div>
   );
 }
